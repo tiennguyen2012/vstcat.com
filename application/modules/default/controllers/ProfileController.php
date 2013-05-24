fe31766f28a6c79e750c371fd1886927cfd029e6
tiennguyen2012 <NGUYENVANTIEN2009@GMAIL.COM>
2013-05-25 00:23:09 +0700

    Change password

---
+++ application/modules/default/controllers/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Tien
+ * Date: 5/24/13
+ * Time: 11:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Default_ProfileController extends Coco_Controller_Action_Default {
+
+    public function init(){
+        parent::init();
+    }
+
+    public function profileAction(){
+        
+    }
+}
